bf3c6031a46bcf06e907a3882ae4b852a8eba2c3
ashisheligocs <84785617+ashisheligocs@users.noreply.github.com>
2024-03-11 18:30:51 +0530

    Init comm

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+  
+    <div class="jumbotron logo">
+      Kanban
+    </div>
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from "./components/Hello";
+
+export default {
+  name: "app",
+  components: {
+    Hello
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+#app .logo {
+  text-align: center;
+}
+</style>
